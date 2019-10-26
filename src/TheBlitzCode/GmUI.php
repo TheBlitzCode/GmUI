6b8107397c1307021c71603bbc95b76535e0a259
TheBlitzCode <56805984+TheBlitzCode@users.noreply.github.com>
2019-10-27 00:03:28 +0200

    Update GmUI.php

--- src/TheBlitzCode/GmUI.php
+++ src/TheBlitzCode/GmUI.php
@@ -1,3 +1,36 @@
 <?php
+/**
+ * GmUI Created by TheBlitzCode
+*/
 
+namespace TheBlitzCode;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\PlayerJoinEvent;
+
+class Main extends PluginBase implements Listener{
+  public function onEnable(){
+  }
+  
+  public function onDisable(){
+  }
+  
+  public function GmUI($player){
+  }
+  
+  public function onCommand(CommandSender $player, Command $cmd, string $label, array $args): bool{
+    if ($cmd->getName() == "gm" or "g"){
+      if ($player->hasPermission("command.gmui")){
+        if ($player instanceof Player){
+          $this->GmUI($player);
+        }
+      }
+    }
+    retuen false;
+  }
+}
 ?>
